988fd09d89605e00b39921e065cacff131ea9ee2
Florian Gramsch <florigram@gmail.com>
2024-06-05 19:19:15 +0200

    basic stuff

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+import EquipList from "./components/EquipList.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <EquipList name="McFitty"></EquipList>
 </template>
 
 <style scoped>
---
+++ frontend/src/components/EquipList.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps({
+  name: String,
+});
+
+const toll = "toll";
+
+const redHTML = `<span style="color: red">This is red</span>`;
+const id = "sameId";
+const zuweisung = {
+  id: "testId",
+  class: "testClass",
+};
+</script>
+
+<template>
+  {{ name }}: {{ toll }} und <span v-html="redHTML"></span>
+  <div v-bind="zuweisung">{{ name }}</div>
+  <div :id>{{ name }}</div>
+</template>
